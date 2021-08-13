02077df847c7d622c898e602f940f483dbb0b4d7
Anderson Cançado <andycancado@gmail.com>
2021-08-13 13:40:24 +0100

    create swift package scafold

---
+++ Tests/polyglot-ios-sdkTests/polyglot_ios_sdkTests.swift
@@ -0,0 +1,11 @@
+    import XCTest
+    @testable import polyglot_ios_sdk
+
+    final class polyglot_ios_sdkTests: XCTestCase {
+        func testExample() {
+            // This is an example of a functional test case.
+            // Use XCTAssert and related functions to verify your tests produce the correct
+            // results.
+            XCTAssertEqual(polyglot_ios_sdk().text, "Hello, World!")
+        }
+    }
